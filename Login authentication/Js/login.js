1861873fa395c6c9ade731612a3c63a49ddd8fc6
Tresor101 <tresorclito1995@gmail.com>
2022-05-09 21:05:36 +0800

    Authentication Js

--- Login authentication/Js/login.js
+++ Login authentication/Js/login.js
@@ -1,11 +1,10 @@
-function verify(){
-    var userEmail = document.getElementById("userEmail").value;
-    var userPassword = document.getElementById("usePassword").value;
-
-    if (userEmail == userPassword){
-        alert("LogIn Successfully");
-        return false;
+function verification(){
+    const userEmail = document.getElementById('userEmail').value;
+    const password = document.getElementById('password').value;
+    if (userEmail == password){
+        alert("LogIn fiontonton");
     }else{
         alert("Unsuccessful LogIn");
     }
-}
+} 
+
