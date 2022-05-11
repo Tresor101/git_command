2715dac1fea1ff93fb1f780653ac397e4d50d3e9
Tresor101 <tresorclito1995@gmail.com>
2022-05-11 13:03:40 +0800

    calculator UI

--- Login authentication/Js/login.js
+++ Login authentication/Js/login.js
@@ -1,6 +1,6 @@
 function verification(){
-    const userEmail = document.getElementById('userEmail').value;
-    const password = document.getElementById('password').value;
+    const userEmail = document.getElementById('userEmail1').value;
+    const password = document.getElementById('password1').value;
     if (userEmail == password){
         alert("LogIn Successfuln");
     }else{
